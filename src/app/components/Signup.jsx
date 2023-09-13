6ce2396727dc2072a369a4b488bac5a9b64dc9b6
MaksymZerkakh <31105146+MaksymZerkakh@users.noreply.github.com>
2023-09-12 23:05:34 -0700

    Fixing test error

--- src/app/components/Signup.jsx
+++ src/app/components/Signup.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { WrapperContainer, RowContainer, CustomButton } from '../components';
 
 const Signup = () => {
   return (
     <section className="py-20">
       <WrapperContainer>
-        <h2 className="uppercase text-center text-5xl">Don't Forget To Sign Up</h2>
+        <h2 className="uppercase text-center text-5xl">Don&apos;t Forget To Sign Up</h2>
         <p className="text-center pt-10">Find out early about all upcoming promotions and new product releases with our newsletter.</p>
         <RowContainer>
           <div className="mx-auto pt-5">
@@ -15,7 +15,7 @@ const Signup = () => {
         </RowContainer>
       </WrapperContainer>
     </section>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
