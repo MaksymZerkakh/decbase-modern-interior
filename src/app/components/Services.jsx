79eef4f6b5af7f42fdf965328af209b4e31be69b
MaksymZerkakh <31105146+MaksymZerkakh@users.noreply.github.com>
2023-09-18 12:24:56 -0700

    Changing the slider functionality and look

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -5,8 +5,6 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { sliderContent } from '../constants';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronCircleLeft, faChevronCircleRight, fas } from '@fortawesome/free-solid-svg-icons';
 import { WrapperContainer } from '../components';
 
 
@@ -38,16 +36,13 @@ const Services = () => {
         speed: 500,
         centerMode: true,
         variableWidth: true,
-        //slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: false,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
                     slidesToScroll: 1,
-                    variableWidth: false,
                 },
             },
             {
--- src/app/components/Testimonials.jsx
+++ src/app/components/Testimonials.jsx
@@ -5,8 +5,6 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { testimonalSlieder } from '../constants';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronCircleLeft, faChevronCircleRight, faQuoteRightAlt } from '@fortawesome/free-solid-svg-icons';
 import { WrapperContainer } from '../components';
 import Image from 'next/image';
 
@@ -15,25 +13,24 @@ const Testimonials = () => {
     const sliderRef = useRef(null);
     const [currentSlide, setCurrentSlide] = useState(0);
 
-
     const handleNext = () => {
         if (currentSlide < testimonalSlieder.length - 1) {
-          sliderRef.current.slickNext(); // Slide to the next slide
+            sliderRef.current.slickNext(); // Slide to the next slide
         } else {
-          sliderRef.current.slickGoTo(0); // Go back to the first slide
+            sliderRef.current.slickGoTo(0); // Go back to the first slide
         }
-      };
-      
-      const handlePrevious = () => {
+    };
+
+    const handlePrevious = () => {
         if (currentSlide > 0) {
-          sliderRef.current.slickPrev(); // Slide to the previous slide
+            sliderRef.current.slickPrev(); // Slide to the previous slide
         } else {
-          sliderRef.current.slickGoTo(testimonalSlieder.length - 1); // Go to the last slide
+            sliderRef.current.slickGoTo(testimonalSlieder.length - 1); // Go to the last slide
         }
-      };
+    };
 
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 2,
@@ -42,7 +39,6 @@ const Testimonials = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
                     slidesToScroll: 1,
                 },
             },
@@ -64,9 +60,33 @@ const Testimonials = () => {
                     <h2 className="text-center text-5xl lg:text-6xl font-light mb-8 z-10 relative">See Whay Our Clients Say About Us</h2>
                 </div>
 
-                <div className="flex justify-end pr-5 slider-con pb-5">
-                    <div className="ml-2 flex mr-5"><span className="text-4xl font-extralight">{(currentSlide + 1).toString().padStart(2, '0')}</span> <span className="font-extralight text-xl">/</span> <span className="font-extralight text-xl">{testimonalSlieder.length.toString().padStart(2, '0')}</span></div>
-                    <div className="slider_nav">
+                <Slider 
+                ref={sliderRef} 
+                {...settings}
+                beforeChange={(oldIndex, newIndex) => setCurrentSlide(newIndex)}
+                >
+                    {testimonalSlieder.map((slide, index) => (
+                        <div key={index} className="px-1">
+                            <div className="slider px-8 py-10">
+                                <div className="relative">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="text-4xl absolute right-0 quote" fill="#CAA892" width="24" height="24" viewBox="0 0 24 24"><path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z"/></svg>
+                                </div>
+                                <h2 className="pb-3 text-lg font-semibold">{slide.name} <span className="text-sm font-light">/ {slide.position}</span></h2>
+                                <p>{slide.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+
+                <div className="flex justify-center m-auto mt-10">
+                    <div className="slider_line">
+                        <span style={{ left: `${currentSlide * (100 / testimonalSlieder.length)}%` }}></span>
+                    </div>
+                </div>
+                
+
+                <div className="flex justify-end pr-5 slider-con pb-5 mt-10">
+                    <div className="slider_nav w-full justify-between">
                         <div className="mr-2" onClick={handlePrevious}>
                             <button className="nav_prev">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="41" height="16.465" viewBox="0 0 41 16.465">
@@ -82,6 +102,7 @@ const Testimonials = () => {
                                 Previous
                             </button>
                         </div>
+                        <div className="mx-2 flex"><span className="text-4xl font-extralight">{(currentSlide + 1).toString().padStart(2, '0')}</span> <span className="font-extralight text-xl">/</span> <span className="font-extralight text-xl">{testimonalSlieder.length.toString().padStart(2, '0')}</span></div>
                         <div onClick={handleNext}>
                             <button className="nav_next">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="41" height="16.465" viewBox="0 0 41 16.465">
@@ -100,24 +121,6 @@ const Testimonials = () => {
                     </div>
                 </div>
 
-
-                <Slider 
-                ref={sliderRef} 
-                {...settings}
-                beforeChange={(oldIndex, newIndex) => setCurrentSlide(newIndex)}
-                >
-                    {testimonalSlieder.map((slide, index) => (
-                        <div key={index} className="px-1">
-                            <div className="slider px-8 py-10">
-                                <div className="relative">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="text-4xl absolute right-0 quote" fill="#CAA892" width="24" height="24" viewBox="0 0 24 24"><path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z"/></svg>
-                                </div>
-                                <h2 className="pb-3 text-lg font-semibold">{slide.name} <span className="text-sm font-light">/ {slide.position}</span></h2>
-                                <p>{slide.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                </Slider>
             </WrapperContainer>
         </section>
     )
